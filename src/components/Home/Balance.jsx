0810f77a16a185a3358c497d06af937b65f9ecb9
omid2007hope <omid85hope@gmail.com>
2025-10-26 01:54:26 +0200

    Margin and Padding Fixes

--- src/components/Home/Balance.jsx
+++ src/components/Home/Balance.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import MyPhoto from "../../assets/Images/MyPhoto.jpg";
 export default function Balance() {
   return (
-    <div className="px-0 sm:px-0 md:px-16 lg:px-25 py-0 sm:py-0 md:py-2 lg:py-2 mx-2 sm:mx-2 md:mx-4 lg:mx-4 my-4 sm:my-4 md:my-4 lg:my-4 flex items-center relative overflow-hidden rounded-3xl bg-[#0F1629] shadow-2xl ring-1 ring-white/5">
-      <img src={MyPhoto} alt="" className="size-30 rounded-4xl " />
-      <div className="flex flex-col justify-center items-center">
+    <div className="px-0 sm:px-0 md:px-3 lg:px-3 py-0 sm:py-0 md:py-2 lg:py-2 mx-2 sm:mx-2 md:mx-4 lg:mx-4 my-4 sm:my-4 md:my-4 lg:my-4 flex flex justify-between items-center relative overflow-hidden rounded-3xl bg-[#0F1629] shadow-2xl ring-1 ring-white/5">
+      <img src={MyPhoto} alt="" className="w-40 rounded-4xl lg:mr-10" />
+      <div className="flex flex-col justify-center items-center md:pl-10 lg:pr-10 md:pr-0 lg:pl-10">
         <div className="flex flex-row justify-center items-center p-4">
           <p className="text-3xl text-white font-bold">Omid Teimory</p>
         </div>
--- src/components/Home/Main.jsx
+++ src/components/Home/Main.jsx
@@ -4,7 +4,7 @@ import BalancePreview from "./BalancePreview";
 export default function Main() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#0F1629] text-white">
-      <div className="flex flex-col justify-center items-center border-1 rounded-3xl">
+      <div className="flex flex-col justify-center items-center relative overflow-hidden rounded-3xl bg-[#0F1629] shadow-2xl ring-1 ring-white/5">
         <Balance />
         <BalancePreview />
       </div>
