f366537c52f92575623ca2b3a21abd45287cdd69
omid2007hope <omid85hope@gmail.com>
2025-10-29 00:50:58 +0100

    Add btn update

--- src/components/Wallet/Manage.jsx
+++ src/components/Wallet/Manage.jsx
@@ -3,7 +3,7 @@ import Balance from "../Home/Balance";
 
 export default function Manage() {
   return (
-    <div className="relative w-screen h-screen flex flex-cols justify-center bg-[#0B1220] text-gray-200">
+    <div className="w-screen h-screen flex flex-cols justify-center bg-[#0B1220] text-gray-200">
       <div className="py-14 sm:py-14 md:py-14 lg:py-14 px-0 sm:px-0 md:px-0 lg:px-0">
         <div className="rounded-2xl border border-white/10 bg-[#0F1629] mb-2">
           <div className="text-3xl font-bold">
@@ -12,11 +12,9 @@ export default function Manage() {
           <div className="rounded-2xl border border-white/10 bg-[#0F1629] p-10 m-2"></div>
         </div>
       </div>
-      <div className="absolute top-200 left-200 z-50 flex flex-col justify-center items-center">
-        <button className="flex flex-col justify-center items-center rounded-full bg-green-500 pb-7 pt-5 px-8">
-          <div className="flex flex-col justify-center items-center text-white text-4xl font-bold">
-            +
-          </div>
+      <div className="fixed top-205 left-205 z-50">
+        <button className="rounded-full bg-green-500/80 border-5 border-green-950/80 pb-5.5 pt-4 px-7 hover:bg-green-500/70 active:bg-green-500/100">
+          <div className="text-white text-4xl font-bold">+</div>
         </button>
       </div>
     </div>
