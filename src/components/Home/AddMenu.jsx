90495d8ba395daecb256a8a3495a7f67b7c7a074
omid2007hope <omid85hope@gmail.com>
2025-10-29 20:51:56 +0100

    Add Menu Update

--- src/components/Home/AddMenu.jsx
+++ src/components/Home/AddMenu.jsx
@@ -27,43 +27,47 @@ export default function AddMenu() {
               className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
             >
               <div className="w-full h-full flex flex-col justify-center items-center bg-[#0B1220] rounded-3xl boreder-3">
-                <div className="w-full h-full flex flex-row justify-between items-center">
-                  <div className="w-full h-full bg-white/80 border-3 rounded-3xl flex flex-row justify-center items-center">
+                <div className="w-full h-full flex flex-row justify-between items-center border-b-1 border-white rounded-md py-6">
+                  <div className="px-8 py-6.5 bg-white/80 border-3 border-green-300 rounded-full flex flex-row justify-center items-center ml-5">
                     O
                   </div>
                   <input
                     type="text"
-                    className="w-full h-full bg-white/80 border-3 rounded-3xl flex flex-row justify-center items-center"
+                    className="px-22 py-1.5 mr-5 bg-green-50 border-3 border-green-300 rounded-md flex flex-row justiy-center items-center"
                   />
                 </div>
-                <div className="w-full h-full flex flex-row justify-between items-center">
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    1
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    2
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    3
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    4
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    5
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    6
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    7
-                  </button>
-                  <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
-                    8
-                  </button>
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                  <div className="w-full h-full gap-6 px-5 py-5 my flex flex-row justify-evenly items-center">
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      1
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      2
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      3
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      4
+                    </button>
+                  </div>
+                  <div className="w-full h-full gap-6 px-5 py-5 my flex flex-row justify-evenly items-center">
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      5
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      6
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      7
+                    </button>
+                    <button className="w-full h-full px-5 py-4.5 flex flex-row justify-around items-center bg-white rounded-full">
+                      8
+                    </button>
+                  </div>
                 </div>
-                <div className="w-full h-full flex flex-row justify-evenly items-center">
-                  <button className="w-full h-full flex flex-row justify-center items-center bg-green-500 rounded-3xl">
+                <div className="w-full h-full flex flex-row justify-evenly items-center border-t-1 border-white rounded-md py-6">
+                  <button className="px-55 py-1.5 flex flex-row justify-center items-center bg-green-500 rounded-md">
                     Add
                   </button>
                 </div>
