86f9fc991abeb4c3787b2e1c0820920a60119d79
omid2007hope <omid85hope@gmail.com>
2025-10-29 04:22:38 +0100

    Card Background

--- src/components/Wallet/Card.jsx
+++ src/components/Wallet/Card.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import Chip from "../../assets/Images/Chip.png";
+import Background from "../../assets/Images/Background.png";
 
 export default function Card() {
   return (
-    <div className="w-full h-full flex flex-col justify-evenly items-center bg-[#0B1220] text-white rounded-3xl">
-      <div className="w-full flex flex-row justify-evenly items-center pb-5">
-        <img src={Chip} alt="" className="w-15 h-11 rounded-lg mr-55 mt-6" />
-        <p className="text-3xl font-bold mt-4">Bank</p>
-      </div>
-      <div className="w-full flex flex-row justify-evenly items-center pt-8 pb-8 text-3xl font-bold">
-        <div className="flex flex-row justify-evenly items-center">
-          <p className="px-2.5">2134</p>
-          <p className="px-2.5">5678</p>
-          <p className="px-2.5">8930</p>
-          <p className="px-2.5">9800</p>
+    <div
+      className="text-white rounded-3xl bg-cover bg-center border-1"
+      style={{ backgroundImage: `url(${Background})` }}
+    >
+      <div className="w-full h-full flex flex-col justify-evenly items-center text-white rounded-3xl">
+        <div className="w-full flex flex-row justify-evenly items-center pb-5">
+          <img src={Chip} alt="" className="w-15 h-11 rounded-lg mr-55 mt-6" />
+          <p className="text-3xl font-bold mt-4">Bank</p>
         </div>
-      </div>
-      <div className="w-full flex flex-row justify-evenly items-center pb-7 pt-8">
-        <p className="text-1xl font-bold ">***</p>
-        <p className="text-1xl font-bold px-18">12/30</p>
-        <p className="text-1xl font-bold ">Omid Teimory</p>
+        <div className="w-full flex flex-row justify-evenly items-center pt-8 pb-8 text-3xl font-bold">
+          <div className="flex flex-row justify-evenly items-center">
+            <p className="px-2.5">2134</p>
+            <p className="px-2.5">5678</p>
+            <p className="px-2.5">8930</p>
+            <p className="px-2.5">9800</p>
+          </div>
+        </div>
+        <div className="w-full flex flex-row justify-evenly items-center pb-7 pt-8">
+          <p className="text-1xl font-bold ">***</p>
+          <p className="text-1xl font-bold px-18">12/30</p>
+          <p className="text-1xl font-bold ">Omid Teimory</p>
+        </div>
       </div>
     </div>
   );
