4157be59a19780a8d469aa7f4355499fcdfa9879
omid2007hope <omid85hope@gmail.com>
2025-11-01 15:11:17 +0100

    Update

--- src/components/Home/Items.jsx
+++ src/components/Home/Items.jsx
@@ -10,19 +10,19 @@ export default function Items({
   const [icon, setIcon] = useState(x);
   return (
     <div className="w-full h-full flex flex-row justify-between items-center rounded-3xl bg-[#0F1629] shadow-2xl rounded-2xl ring-1 ring-white/5">
-      <div className="w-full h-full flex flex-row justify-center items-center py-8 border-r-1">
+      <div className="w-1/4 flex flex-row justify-center items-center py-8 border-r-1">
         <div className="w-full h-full flex flex-row justify-center items-center">
           {icon}
         </div>
       </div>
-      <div className="flex flex-col items-center bg-[#0F1629] ">
-        <div className="flex flex-row items-center py-2 pr-62">
+      <div className="w-3/4 flex flex-col items-center bg-[#0F1629]">
+        <div className="w-full flex flex-row justify-start items-center py-2">
           <p className="text-1xl font-bold rounded-md ml-2.5">{title}</p>
           <button className="bg-green-500/50 hover:bg-green-500/20 active:bg-green-400/50 text-green-400 border-1 border-green-400 rounded-md px-0.5 ml-2.5 mr-2.5">
             Edit
           </button>
         </div>
-        <div className="flex flex-row items-center py-2 pl-60 border-t-1">
+        <div className="w-full flex flex-row justify-end items-center py-2 border-t-1">
           <p
             className="
           text-sm font-bold rounded-md"
